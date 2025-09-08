6a865a536da0280e497f2239be01f7aa91302437
MacIP.net <info@macip.net>
2025-09-08 17:55:28 +0200

    Small edits - latest Mini Browser version 1.1

--- sdk_apps/mini_browser/mini_browser.c
+++ sdk_apps/mini_browser/mini_browser.c
@@ -467,9 +467,9 @@ static int fetch_url(const char *url, mem_t *m) {
 /* Build explicit headers (works even if CURLOPT_USERAGENT is ignored) */
 struct curl_slist *hdrs = NULL;
 hdrs = curl_slist_append(hdrs,
-    "User-Agent: Mozilla/5.0 (BadgeVMS; ESP32; rv:2.1) "
-    "Gecko/20100101 BadgeBrowser/1.0 "
-    "(compatible; MiniBrowser/1.0; +https://github.com/mactjaap/mini_browser/; HTTP/1.1; identity)");
+    "User-Agent: Mozilla/5.0 (BadgeVMS; ESP32; rv:1.1) "
+    "Gecko/20100101 "
+    "(compatible; MiniBrowser/1.1; +https://github.com/mactjaap/mini_browser/; HTTP/1.1; identity)");
 hdrs = curl_slist_append(hdrs,
     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 hdrs = curl_slist_append(hdrs, "Accept-Language: en-US,en;q=0.5");
